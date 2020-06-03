057cc5cca00f772e48349d6be48e5d53a108e1c8
RaulAguilar <RaulAguilar30@gmail.com>
2020-06-03 13:20:10 -0500

    Agregar vistas de triaje y detalle en la web

--- Web/Coronavirus_Aplicacion/Coronavirus_Aplicacion/Controllers/ListadoController.cs
+++ Web/Coronavirus_Aplicacion/Coronavirus_Aplicacion/Controllers/ListadoController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Data;
 using System.Data.Entity;
 using System.Linq;
+using System.Net;
 using System.Runtime.Remoting.Contexts;
 using System.Threading.Tasks;
 using System.Web;
@@ -14,23 +15,61 @@ namespace Coronavirus_Aplicacion.Controllers
 {
     public class ListadoController : Controller
     {
-        private sars_cov_2Entities1 bd = new sars_cov_2Entities1();
+        private sars_cov_2Entities bd = new sars_cov_2Entities();
 
         // GET: Listado
 
         public ActionResult Listado(ciudadano model)
         {
-            ViewBag.tipo_documento = new SelectList(bd.tipo_documento, "idTipoDocumento", "desTipoDocumento");
-            return View(bd.ciudadano.ToList());
+
+            ViewBag.estado = new SelectList(bd.tipo_estado, "idestado", "estado");
+            return View(bd.estado_ciudadano.ToList());
+
+            //ViewBag.estado = new SelectList(bd.tipo_estado, "idestado", "estado");
+            //return View(bd.estado_ciudadano.ToList());
         }
 
         [HttpPost]
-        public ActionResult Listado(int tipo_documento)
+        public ActionResult Listado(int estado)
         {
-            ViewBag.tipo_documento = new SelectList(bd.tipo_documento, "idTipoDocumento", "desTipoDocumento");
-            var tipo = bd.ciudadano.Include(x => x.tipo_documento).Where(a => a.idtipodocumento == tipo_documento);
+            ViewBag.estado = new SelectList(bd.tipo_estado, "idestado", "estado");
+            var tipo = bd.estado_ciudadano.Include(x => x.tipo_estado).Where(a => a.idestado == estado);
             return View(tipo.ToList());
 
+        }
+        
+
+        public ActionResult Detalle(int? doc)
+        {
+            if (doc == null)
+            { 
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                
+            }
+            ciudadano obj = bd.ciudadano.Find(doc);
+
+            if (obj == null)
+            {
+                HttpNotFound();
+            }
+            return View(obj);
+        }
+
+
+        public ActionResult Triaje(int? docc)
+        {
+            if (docc == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+
+            }
+            var rdj = bd.triaje.FirstOrDefault(x => x.docciudadano == docc);
+
+            if (rdj == null)
+            {
+                HttpNotFound();
+            }
+            return View(rdj);
         }
 
 
